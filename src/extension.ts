b0cb4750060fc1803aaabb0bb7b21ed1cdc9ebab
Tomás Migone <tomas@balena.io>
2020-10-07 14:31:22 -0300

    Use context subscriptions to dispose of commands.
    Change-type: patch
    Signed-off-by: Tomás Migone <tomas@balena.io>

--- src/extension.ts
+++ src/extension.ts
@@ -6,51 +6,45 @@ import * as scan from './lib/scan';
 import BalenaDevicesDataProvider, { BalenaDeviceItem } from './providers/balena-devices-treedata';
 
 (async () => {
-    await scan.initialized;
+	await scan.initialized;
 })();
 
-let livepushCommand: vscode.Disposable | undefined;
-let sshCommand: vscode.Disposable | undefined;
-
 export function activate(context: vscode.ExtensionContext) {
-		
 	vscode.window.registerTreeDataProvider('balenaDevices', new BalenaDevicesDataProvider());
 
-	livepushCommand = vscode.commands.registerCommand('balena.livePush', ({ addresses }: BalenaDeviceItem) => {
-		const [ address ] = addresses.filter(a => !a.includes(':'));
-		if (!address) {
-			return;
-		}
+	context.subscriptions.push(
+		vscode.commands.registerCommand('balena.livePush', ({ addresses }: BalenaDeviceItem) => {
+			const [address] = addresses.filter(a => !a.includes(':'));
+			if (!address) {
+				return;
+			}
 
-		const [workspace] = vscode.workspace.workspaceFolders!;
-        if (!workspace) {
-            return;
-        }
+			const [workspace] = vscode.workspace.workspaceFolders!;
+			if (!workspace) {
+				return;
+			}
 
-		const task = new vscode.Task(
-			{ type: 'livepush' },
-			workspace,
-			'livepush',
-			'livepush',
-			new vscode.ShellExecution(`balena push ${address} -m`)
-		);
-		vscode.tasks.executeTask(task);
-	});
+			const task = new vscode.Task(
+				{ type: 'livepush' },
+				workspace,
+				'livepush',
+				'livepush',
+				new vscode.ShellExecution(`balena push ${address} -m`)
+			);
+			vscode.tasks.executeTask(task);
+		})
+	);
 
-	sshCommand = vscode.commands.registerCommand('balena.ssh', ({ addresses }: BalenaDeviceItem) => {
-		const [ address ] = addresses.filter(a => !a.includes(':'));
-		if (!address) {
-			return;
-		}
-		
-		const terminal = vscode.window.createTerminal('SSH');
-		terminal.show();
-		terminal.sendText(`balena ssh ${address}`);
-	});
-}
+	context.subscriptions.push(
+		vscode.commands.registerCommand('balena.ssh', ({ addresses }: BalenaDeviceItem) => {
+			const [address] = addresses.filter(a => !a.includes(':'));
+			if (!address) {
+				return;
+			}
 
-// this method is called when your extension is deactivated
-export function deactivate() {
-	livepushCommand?.dispose();
-	sshCommand?.dispose();
+			const terminal = vscode.window.createTerminal('SSH');
+			terminal.show();
+			terminal.sendText(`balena ssh ${address}`);
+		})
+	);
 }
